cd6e8951bb37b9cece2b64e85409d37aad84e784
sar <saaryal51@gmail.com>
2019-08-10 06:52:30 +0545

    Some New Changes.

--- automate.py
+++ automate.py
@@ -21,16 +21,18 @@ dir_path = "/".join(sys.argv[0].split("/")[:-1])
 if not dir_path:
     dir_path = os.getcwd()
 # Project Title Creation
-projet_title = Label(text="Youtube Automation")
+projet_title = Label(text="YOUTUBE COMMENT LIKER", bg="WHITE")
 
 
 # Channel Url Field Label
-channel_url_label = Label(text="Channel URL")
+channel_url_label = Label(text="ENTER CHANNEL URL", bg="white")
 
 # Channel Url Field Creation
 channel_url_var = tk.StringVar()
-channel_url_entry = tk.Entry(root, textvariable=channel_url_var)
+channel_url_entry = tk.Entry(root, textvariable=channel_url_var, width=40, highlightcolor="black", highlightthickness=1)
 
+# Activate Button
+activate_button = tk.Button(root, text="ACTIVATE")
 
 # VideoList Label
 video_list_label = tk.Label(text="Video List")
@@ -51,7 +53,10 @@ output_box = Listbox(root)
 output_box.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=output_box.yview)
 
+start_auto_like = tk.Button(root, text='Start Auto Like', bg="green", activebackground="#001C7C")
+
 driver = None
+
 
 def delay(n, fixed=False):
     if fixed:
@@ -63,7 +68,10 @@ def start_automation(re_initialize=False):
     try:
         global driver
         # Selenium Driver Initilization Work
-        chromedriver = os.path.join(sys._MEIPASS, "chromedriver.exe")
+        try:
+            chromedriver = os.path.join(sys._MEIPASS, "chromedriver.exe")
+        except:
+            chromedriver = os.path.join(dir_path, "chromedriver.exe")
         if re_initialize or driver is None:
             chrome_options = webdriver.ChromeOptions()
             chrome_options.add_argument("--mute-audio")
@@ -107,39 +115,39 @@ def submit():
 
 def create_initial_screen():
     row = 0
-    projet_title.grid(column=0, row=row, sticky=N + S + W + E, columnspan=2)
+    projet_title.grid(column=1, row=row, sticky=N + S + W + E, padx=20, pady=10)
+    activate_button.grid(column=2, row=row, sticky=N + S + W + E, padx=20, pady=10)
     row += 1
-    channel_url_label.grid(column=0, row=row, sticky=N + S + W + E)
-    output_box_label.grid(column=1, row=row, sticky=N + S + W + E)
-    row += 1
-    channel_url_entry.grid(column=0, row=row, sticky=N + S + W + E)
-    output_box.grid(column=1, row=row, sticky=N + S + W + E, rowspan=5)
-    root.grid_rowconfigure(row, weight=1)
-    row += 1
-    submit_button = tk.Button(root, text='Submit', bg="green", fg='white', activebackground="#001C7C")
+    channel_url_label.grid(column=0, row=row, sticky=N + S + W + E, padx=20, pady=(20, 10))
+    channel_url_entry.grid(column=1, row=row, sticky=N + S + W + E, pady=(20, 10))
+    submit_button = tk.Button(root, text='IMPORT VIDEOS', bg="green", fg='white', activebackground="#001C7C")
     submit_button['command'] = lambda: submit()
-    submit_button.grid(column=0, row=row, sticky=N + S + W + E)
+    submit_button.grid(column=2, row=row, sticky=N + S + W + E, padx=20, pady=(20, 10))
     row += 1
-    video_list_label.grid(column=0, row=row, sticky=N + S + W + E)
-    row += 1
-    video_list.grid(column=0, row=row, sticky=N + S + W + E)
-    root.grid_rowconfigure(row, weight=10)
-    row += 1
-    root.grid_columnconfigure(0, weight=4)
-    root.grid_columnconfigure(1, weight=2)
+    video_list.grid(column=0, row=row, sticky=N + S + W + E, rowspan=5, columnspan=2, padx=(20, 0), pady=(10, 20))
+    output_box.grid(column=2, row=row, sticky=N + S + W + E, rowspan=6, padx=(0, 20), pady=(10, 20))
+    start_auto_like.grid(column=0, row=7, sticky=N + S + W + E, pady=(0, 20), columnspan=2, padx=(20, 0))
+    for r in range(7):
+        root.grid_rowconfigure(row, weight=1)
+    root.grid_columnconfigure(0, weight=2)
+    root.grid_columnconfigure(1, weight=1)
+    root.grid_columnconfigure(2, weight=2)
+    # root.grid_columnconfigure(1, weight=2)
 
 
 def start_like_process(videos_url):
     try:
         driver.maximize_window()
+        driver.set_window_position(0, 0)
+        driver.set_window_size(400, 400)
         urls = [videos_url[idx] for idx in video_list.curselection()]
         for url in urls:
             like_count = 0
-            output_box.insert(END, "Opning URL %s for auto like" % url)
+            output_box.insert(END, "Opening URL %s for auto like" % url)
             driver.get(url)
             delay(5)
             previous_buttons = []
-            scroll_to = 600
+            scroll_to = 3600
             driver.execute_script("window.scrollTo(0, %s);" % str(scroll_to))
             iter_check = 0
             while True:
@@ -173,29 +181,29 @@ def start_auto_like_method(videos_url):
     th = threading.Thread(target=start_like_process, args=(videos_url,))
     th.start()
 
+
 def stop_auto_like(th):
     th.stop()
 
+
 def fetch_video_urls(channel_url):
+    videos_url = []
     try:
         start_automation()
         output_box.insert(END, "Fetching Video URL for youtube channel: %s" % channel_url)
         driver.get(channel_url)
         WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.ID, "video-title")))
         links = driver.find_elements_by_id("video-title")
-        videos_url = []
         video_list.delete(0, END)
         for i, x in enumerate(links):
             text = x.get_attribute("text")
             href = x.get_attribute("href")
             videos_url.append(href)
             video_list.insert(END, str(i+1)+": "+text)
-        start_auto_like = tk.Button(root, text='Start Auto Like', bg="green", fg='white', activebackground="#001C7C")
-        start_auto_like['command'] = lambda: start_auto_like_method(videos_url)
-        start_auto_like.grid(column=0, row=6, sticky=N + S + W + E)
         output_box.insert(END, "Fetching Video Completed.")
     except Exception as ex:
         output_box.insert(END, str(ex))
+    start_auto_like['command'] = lambda: start_auto_like_method(videos_url)
 
 
 def on_closing():
@@ -211,10 +219,10 @@ def on_closing():
 def main():
     try:
         # Set window size to 480x640
-        root.geometry("1000x700")
+        root.geometry("800x500")
         # root.resizable(False, False)
-        root.winfo_toplevel().title("Youtube Automation")
-        root["bg"] = "#292B39"
+        root.winfo_toplevel().title("YOUTUBE COMMENT LIKER")
+        root["bg"] = "#ffffff"
         create_initial_screen()
         root.protocol("WM_DELETE_WINDOW", on_closing)
         root.mainloop()
